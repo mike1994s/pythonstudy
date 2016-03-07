56ae8d388e9f7e13f1173e029b0643ad7ee26520
Misha <meshuin94@gmail.com>
2016-03-08 01:16:45 +0400

    first

---
+++ module1/tcpEchoServer.py
@@ -0,0 +1,14 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(10)
+while True:
+	conn, addr = s.accept()
+	while True:
+		data = conn.recv(1024)
+		if not data: break
+		#print "%s" %data
+		if data == 'close':
+			conn.close()
+			break
+		conn.send(data)
